86ede06b3c6f58736ba8186b05a3b5960aca6c14
LuJia <losejia@outlook.com>
2018-09-16 23:01:37 +0800

    add zookeeper cluster

--- dubbo-demo-provider/src/main/java/com/jia/dubbodemo/provider/bootstrap/DubboDemoProviderApplication.java
+++ dubbo-demo-provider/src/main/java/com/jia/dubbodemo/provider/bootstrap/DubboDemoProviderApplication.java
@@ -1,9 +1,11 @@
 package com.jia.dubbodemo.provider.bootstrap;
 
+import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@DubboComponentScan("com.jia.dubbodemo.provider")
 public class DubboDemoProviderApplication {
 
     public static void main(String[] args) {
